f9c79fdecb62274834dadf1f09002c87644a5db5
winterminute <robint@winterminute.com>
2017-04-08 00:13:49 -0700

    Commit

--- src/main.ino
+++ src/main.ino
@@ -22,7 +22,7 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
-  delay(500);
+  delay(5000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
